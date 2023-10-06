d8904d64ad018440389d02874e643f4e02b58476
vanessalopeznr <vanessalopeznr@gmail.com>
2023-10-06 17:38:31 +0200

    Upload

--- Seance3/EKFLocalization.py
+++ Seance3/EKFLocalization.py
@@ -62,7 +62,11 @@ class Simulation:
         return xnow, u
 
 
-    # generate a noisy observation of a random feature
+    # generate a noisy observation of a random feature(Modelo no linear)
+    #Yt=H*Xt+Wt
+    #H = measurement matrix (used to convert the state into predicted sensor observations)
+    #Xt = state vector
+    #Wt = noise of each of sensor observations
     def get_observation(self, k):
         # Ensuring random repetability for given k
         np.random.seed(seed*3 + k)
@@ -79,6 +83,7 @@ class Simulation:
                 zNoise = np.array([zNoise]).T
                 z = observation_model(self.xTrue, iFeature, self.Map) + zNoise
                 z[1, 0] = angle_wrap(z[1, 0])
+                print("EStoy aqui")
         else:
             z = None
             iFeature = None
@@ -89,22 +94,22 @@ class Simulation:
 
 # evolution model (f)
 def motion_model(x, u, dt_pred):
+    # xPred: La posicion actual = posicion anterior + velocidad*tiempo (diap 27 - Xk)
     # x: estimated state (x, y, heading)
     # u: control input or odometry measurement in body frame (Vx, Vy, angular rate)
-    
-    # ...................
+    xPred=tcomp(x,u,dt_pred)
     
     return xPred
 
-
-# observation model (h)
+# observation model (h):  we use the state to estimate sensor measurements that represents a vector of predicted sensor measurements y
 def observation_model(xVeh, iFeature, Map):
+    #One landmark is randomly selected to simulate a measurement
     # xVeh: vecule state
-    # iFeature: observed amer index
-    # Map: map of all amers
-    
-    # ...................
-    
+    # iFeature: observed amer index (landmark index)
+    # Map: map of all amers (2x30)
+    xP=Map[:,iFeature]
+    xVeh=np.squeeze(xVeh)
+    z=np.array([sqrt((xP[0]-xVeh[0])**2+(xP[1]-xVeh[1])**2),atan2((xP[1]-xVeh[1]),(xP[0]-xVeh[0]))-xVeh[2]])
     return z
 
 
@@ -115,10 +120,11 @@ def get_obs_jac(xPred, iFeature, Map):
     # xPred: predicted state
     # iFeature: observed amer index
     # Map: map of all amers
-    
-    # ...................
-
-    return jH
+    xP=Map[:,iFeature]
+    xPred=np.squeeze(xPred)
+    H=np.array([[-(xP[0]-xPred[0])/sqrt((xP[0]-xPred[0])**2+(xP[1]-xPred[1])**2), -(xP[1]-xPred[1])/sqrt((xP[0]-xPred[0])**2+(xP[1]-xPred[1])**2), 0],
+                [(xP[1]-xPred[1])/((xP[0]-xPred[0])**2+(xP[1]-xPred[1])**2), -(xP[0]-xPred[0])/((xP[0]-xPred[0])**2+(xP[1]-xPred[1])**2), -1]])
+    return H
 
 
 # f(x,u) Jacobian wrt x
@@ -126,9 +132,12 @@ def F(x, u, dt_pred):
     # x: estimated state (x, y, heading)
     # u: control input (Vx, Vy, angular rate)
     # dt_pred: time step
-    
-    # ...................
-
+    #Primera columna es la derivada del modelo Xk con respecto a x, la segnda con respecto a y y la tercera con respecto a theta
+    x=np.squeeze(x)
+    u=np.squeeze(u)
+    Jac=np.array([[1, 0, -u[0]*sin(x[2])*dt_pred-u[1]*cos(x[2])*dt_pred],
+                  [0, 1, u[0]*cos(x[2])*dt_pred-u[1]*sin(x[2])*dt_pred],
+                  [0, 0, 1]])
     return Jac
 
 
@@ -136,9 +145,10 @@ def F(x, u, dt_pred):
 def G(x, u, dt_pred):
     # x: estimated state (x, y, heading) in ground frame
     # u: control input (Vx, Vy, angular rate) in robot frame
-    # dt_pred: time step for prediction
-    
-    # ...................
+    # dt_pred: time step for prediction 
+    Jac=np.array([[cos(x[2])*dt_pred, -sin(x[2])*dt_pred, 0],
+                  [sin(x[2])*dt_pred, cos(x[2])*dt_pred, 0],
+                  [0, 0, dt_pred]])
 
     return Jac
 
@@ -229,10 +239,12 @@ nLandmarks = 30
 Map = 140*(np.random.rand(2, nLandmarks) - 1/2)
 
 # True covariance of errors used for simulating robot movements
+# Datos reales a los cuales el filtro de kalman no tiene acceso
 QTrue = np.diag([0.01, 0.01, 1*pi/180]) ** 2
 RTrue = np.diag([5.0, 6*pi/180]) ** 2
 
 # Modeled errors used in the Kalman filter process
+# Datos que tiene acceso el filtro de kalman
 QEst = 100*np.eye(3, 3) @ QTrue
 REst = 10*np.eye(2, 2) @ RTrue
 
@@ -263,27 +275,29 @@ for k in range(1, simulation.nSteps):
     xOdom, u_tilde = simulation.get_odometry(k)
 
     # Kalman prediction
-    xPred = #...................  # function f
-    PPred = #...................
+    xPred = motion_model(xOdom,u_tilde,dt_pred)  # function f
+    Fmat=F(xOdom,u_tilde,dt_pred)
+    Gmat=G(xOdom,u_tilde,dt_pred)
+    PPred = (np.dot(Fmat,np.dot(PEst,Fmat.T)))+np.dot(Gmat,np.dot(QEst,Gmat.T))
 
     # Get random landmark observation
-    [z, iFeature] = simulation.get_observation(k)
-
+    [z, iFeature] = simulation.get_observation(k) #z dato del sensor (Yt)
+    
     if z is not None:
         # Predict observation
-        zPred = observation_model(xPred, iFeature, Map)
+        zPred = observation_model(xPred, iFeature, Map) #z dato predicho desde el radar y la odometria (h(Xt_hat))
 
         # get observation Jacobian
         H = get_obs_jac(xPred, iFeature, Map)
 
         # compute Kalman gain - with dir and distance
-        Innov = #...................         # observation error (innovation)
+        Innov = z-zPred         # observation error (innovation)
         Innov[1, 0] = angle_wrap(Innov[1, 0])
-        S = #...................
-        K = #...................
+        S = REst + np.dot(H,np.dot(PEst,H.T))
+        K = np.dot(PEst,np.dot(H.   T,np.linalg.inv(S)))
 
-        # Compute Kalman gain to use only distance
-#        Innov = #...................       # observation error (innovation)
+        # Compute Kalman gain to use only distance 
+#        Innov =        # observation error (innovation)
 #        H = #...................
 #        S = #...................
 #        K = #...................
@@ -296,10 +310,10 @@ for k in range(1, simulation.nSteps):
 #        K = #...................
 
         # perform kalman update
-        xEst =  #...................
+        xEst = xPred + np.dot(K,Innov)
         xEst[2, 0] = angle_wrap(xEst[2, 0])
 
-        PEst = #...................
+        PEst = np.dot(np.eye(3)-np.dot(K,H),PPred)
         
         
         PEst = 0.5 * (PEst + PEst.T)  # ensure symetry
@@ -375,3 +389,4 @@ for k in range(1, simulation.nSteps):
 plt.savefig('EKFLocalization.png')
 print("Press Q in figure to finish...")
 plt.show()
+
